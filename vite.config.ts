355a4fbf365e68ff2d850fd588b44ab3b3752d19
Bert <bertmulder@gmail.com>
2025-09-22 07:02:36 +0200

    vitchange

--- vite.config.ts
+++ vite.config.ts
@@ -10,24 +10,24 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      '/api/heygen': {
-        target: 'https://api.heygen.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/heygen/, ''),
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (_proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api/heygen': {
+  //       target: 'https://api.heygen.com',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api\/heygen/, ''),
+  //       configure: (proxy, _options) => {
+  //         proxy.on('error', (err, _req, _res) => {
+  //           console.log('proxy error', err);
+  //         });
+  //         proxy.on('proxyReq', (_proxyReq, req, _res) => {
+  //           console.log('Sending Request to the Target:', req.method, req.url);
+  //         });
+  //         proxy.on('proxyRes', (proxyRes, req, _res) => {
+  //           console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+  //         });
+  //       },
+  //     }
+  //   }
+  // }
 })
