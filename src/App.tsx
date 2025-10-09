636cdd3ef1acaa2b73eab172d81d7dfaf8e4fc53
Bert <bertmulder@gmail.com>
2025-10-09 06:45:28 +0200

    FIX:remove Analyzing

--- src/App.tsx
+++ src/App.tsx
@@ -936,16 +936,6 @@ return (
         </button>
       )}
 
-      {/* Analyzing overlay for camera mode */}
-      {isCameraActive && isAnalyzing && (
-        <div className="absolute inset-0 bg-black/50 flex items-center justify-center z-30">
-          <div className="text-white text-center">
-            <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-white mx-auto mb-4"></div>
-            <div className="text-lg">Analyzing...</div>
-          </div>
-        </div>
-      )}
-
       {/* Loading overlay */}
       {(startLoading || startAvatarLoading) && (
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20">
